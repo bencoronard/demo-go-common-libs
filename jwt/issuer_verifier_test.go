1ad567c2a082f7bc8003face7ee1626d5d20ed21
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-12-10 21:11:24 +0700

    Add unit tests for jwt issuer/verifier constructors

--- jwt/issuer_verifier_test.go
+++ jwt/issuer_verifier_test.go
@@ -5,6 +5,8 @@ import (
 	"crypto/rsa"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 const (
@@ -35,6 +37,22 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func TestHelloWorld(t *testing.T) {
+func TestNewSymmJwtVerifier_withNilSymmetricKey_shouldReturnError(t *testing.T) {
+	_, err := NewSymmJwtVerifier(nil)
+	assert.EqualError(t, err, "symmetric key must not be nil")
+}
 
+func TestNewAsymmJwtVerifier_withNilPublicKey_shouldReturnError(t *testing.T) {
+	_, err := NewAsymmJwtVerifier(nil)
+	assert.EqualError(t, err, "public key must not be nil")
+}
+
+func TestNewSymmJwtIssuer_withNilSymmetricKey_shouldReturnError(t *testing.T) {
+	_, err := NewSymmJwtIssuer(issuerName, nil)
+	assert.EqualError(t, err, "symmetric key must not be nil")
+}
+
+func TestNewAsymmJwtIssuer_withNilPrivateKey_shouldReturnError(t *testing.T) {
+	_, err := NewAsymmJwtIssuer(issuerName, nil)
+	assert.EqualError(t, err, "private key must not be nil")
 }
