f0469015d0a2dc1bea2e064587d6296054857d40
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-12-15 14:34:50 +0700

    Add token malformed error

--- jwt/error.go
+++ jwt/error.go
@@ -6,4 +6,5 @@ var (
 	ErrTokenUnexpectedSignMethod  = errors.New("token has unexpected signing method")
 	ErrTokenClaimsInvalid         = errors.New("token has invalid claims")
 	ErrExpiredTokenIssueAttempted = errors.New("expired token issuance attempted")
+	ErrTokenMalformed             = errors.New("token is malformed")
 )
