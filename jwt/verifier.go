3b8c2af1abfcf3ae36045347bdbf317727acf0a5
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-12-15 14:32:39 +0700

    Improve JWT verifier

--- jwt/verifier.go
+++ jwt/verifier.go
@@ -13,65 +13,67 @@ type JwtVerifier interface {
 }
 
 type unsignedJwtVerifier struct {
+	kf jwt.Keyfunc
 }
 
 type symmJwtVerifier struct {
-	key []byte
+	kf jwt.Keyfunc
 }
 
 type asymmJwtVerifier struct {
-	key *rsa.PublicKey
+	kf jwt.Keyfunc
 }
 
 func NewUnsignedJwtVerifier() (JwtVerifier, error) {
-	return &unsignedJwtVerifier{}, nil
+	return &unsignedJwtVerifier{kf: func(t *jwt.Token) (any, error) {
+		if t.Method != jwt.SigningMethodNone {
+			return nil, fmt.Errorf("%w: %T", ErrTokenUnexpectedSignMethod, t.Method)
+		}
+		return jwt.UnsafeAllowNoneSignatureType, nil
+	}}, nil
 }
 
 func NewSymmJwtVerifier(key []byte) (JwtVerifier, error) {
 	if key == nil {
 		return nil, errors.New("symmetric key must not be nil")
 	}
-	return &symmJwtVerifier{key: key}, nil
+
+	return &symmJwtVerifier{kf: func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("%w: %T", ErrTokenUnexpectedSignMethod, t.Method)
+		}
+		return key, nil
+	}}, nil
 }
 
 func NewAsymmJwtVerifier(key *rsa.PublicKey) (JwtVerifier, error) {
 	if key == nil {
 		return nil, errors.New("public key must not be nil")
 	}
-	return &asymmJwtVerifier{key: key}, nil
-}
-
-func (v *unsignedJwtVerifier) VerifyToken(tokenStr string) (jwt.MapClaims, error) {
-	return verifyToken(tokenStr, func(t *jwt.Token) (any, error) {
-		if t.Method != jwt.SigningMethodNone {
-			return nil, fmt.Errorf("%w: %T", ErrTokenUnexpectedSignMethod, t.Method)
-		}
-		return jwt.UnsafeAllowNoneSignatureType, nil
-	})
-}
-
-func (v *symmJwtVerifier) VerifyToken(tokenStr string) (jwt.MapClaims, error) {
-	return verifyToken(tokenStr, func(t *jwt.Token) (any, error) {
-		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("%w: %T", ErrTokenUnexpectedSignMethod, t.Method)
-		}
-		return v.key, nil
-	})
-}
-
-func (v *asymmJwtVerifier) VerifyToken(tokenStr string) (jwt.MapClaims, error) {
-	return verifyToken(tokenStr, func(t *jwt.Token) (any, error) {
+	return &asymmJwtVerifier{kf: func(t *jwt.Token) (any, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, fmt.Errorf("%w: %T", ErrTokenUnexpectedSignMethod, t.Method)
 		}
-		return v.key, nil
-	})
+		return key, nil
+	}}, nil
 }
 
-func verifyToken(tokenStr string, keyFunc jwt.Keyfunc) (jwt.MapClaims, error) {
+func (v *unsignedJwtVerifier) VerifyToken(tokenStr string) (jwt.MapClaims, error) {
+	return verifyToken(tokenStr, v.kf)
+}
+
+func (v *symmJwtVerifier) VerifyToken(tokenStr string) (jwt.MapClaims, error) {
+	return verifyToken(tokenStr, v.kf)
+}
+
+func (v *asymmJwtVerifier) VerifyToken(tokenStr string) (jwt.MapClaims, error) {
+	return verifyToken(tokenStr, v.kf)
+}
+
+func verifyToken(tokenStr string, kf jwt.Keyfunc) (jwt.MapClaims, error) {
 	claims := jwt.MapClaims{}
 
-	_, err := jwt.ParseWithClaims(tokenStr, claims, keyFunc)
+	_, err := jwt.ParseWithClaims(tokenStr, claims, kf)
 	if err != nil {
 		return nil, err
 	}
