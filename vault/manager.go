7a46f7ad88b79c18489fbea0c0c06f6bdd4a1141
ben@orion <128577990+bencoronard@users.noreply.github.com>
2026-01-13 15:43:48 +0700

    Init vault client manager implementation from Vault

--- vault/manager.go
+++ vault/manager.go
@@ -2,22 +2,67 @@ package vault
 
 import (
 	"context"
-	"sync"
+	"log"
 
+	vault "github.com/hashicorp/vault/api"
 	"go.uber.org/fx"
 )
 
 type VaultClientManager interface {
-	renewToken(ctx context.Context) error
+	renewToken(ctx context.Context)
+	manageTokenLifecycle() error
 }
 
 type vaultClientManager struct {
 	vc Client
-	mu sync.Mutex
 }
 
-func (m *vaultClientManager) renewToken(ctx context.Context) error {
-	return m.vc.authenticate(ctx)
+func (m *vaultClientManager) renewToken(ctx context.Context) {
+	for {
+		_, err := m.vc.authenticate(ctx)
+		if err != nil {
+			log.Fatalf("unable to authenticate to Vault: %v", err)
+		}
+
+		if err := m.manageTokenLifecycle(); err != nil {
+			log.Fatalf("unable to start managing token lifecycle: %v", err)
+		}
+	}
+}
+
+func (m *vaultClientManager) manageTokenLifecycle() error {
+	token, err := m.vc.client().Auth().Token().LookupSelf()
+	if err != nil {
+		return err
+	}
+
+	if !token.Auth.Renewable {
+		log.Printf("Token is not renewable. Re-attempting login")
+		return nil
+	}
+
+	watcher, err := m.vc.client().NewLifetimeWatcher(&vault.LifetimeWatcherInput{})
+	if err != nil {
+		return err
+	}
+
+	go watcher.Start()
+	defer watcher.Stop()
+
+	for {
+		select {
+		case err := <-watcher.DoneCh():
+			if err != nil {
+				log.Printf("Failed to renew token: %v. Re-attempting login", err)
+				return nil
+			}
+
+			log.Printf("Token can no longer be renewed. Re-attempting login.")
+			return nil
+		case renewal := <-watcher.RenewCh():
+			log.Printf("Successfully renewed: %#v", renewal)
+		}
+	}
 }
 
 func NewVaultClientManager(lc fx.Lifecycle, vc Client) (VaultClientManager, error) {
