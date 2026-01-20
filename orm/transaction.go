f7c461de877b0553080bbbc4f4ff7b666b2c92f3
ben@orion <128577990+bencoronard@users.noreply.github.com>
2026-01-20 11:21:05 +0700

    Add context to transactional

--- orm/transaction.go
+++ orm/transaction.go
@@ -1,11 +1,13 @@
 package orm
 
 import (
+	"context"
+
 	"gorm.io/gorm"
 )
 
 type TransactionManager interface {
-	Transactional(fn func(tx *gorm.DB) error) error
+	Transactional(ctx context.Context, fn func(tx *gorm.DB) error) error
 }
 
 type transactionManager struct {
@@ -16,13 +18,8 @@ func NewTransactionManager(db *gorm.DB) (TransactionManager, error) {
 	return &transactionManager{db: db}, nil
 }
 
-func (t transactionManager) Transactional(fn func(tx *gorm.DB) error) error {
-	err := t.db.Transaction(func(tx *gorm.DB) error {
+func (t transactionManager) Transactional(ctx context.Context, fn func(tx *gorm.DB) error) error {
+	return t.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
 		return fn(tx)
 	})
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
