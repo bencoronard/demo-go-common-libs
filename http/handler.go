7eefb778e8dc204a864462d22b1a9ec41859385a
ben@corp <128577990+bencoronard@users.noreply.github.com>
2026-02-10 21:33:54 +0700

    Add default errors to http error handler

--- http/handler.go
+++ http/handler.go
@@ -4,32 +4,39 @@ import (
 	"errors"
 	"log/slog"
 	"net/http"
+	"time"
 
 	"github.com/bencoronard/demo-go-common-libs/constant"
 	"github.com/bencoronard/demo-go-common-libs/dto"
+	"github.com/bencoronard/demo-go-common-libs/jwt"
 	"github.com/labstack/echo/v5"
 )
 
-type ErrorHandlerFunc func(c *echo.Context, err error) error
+type CustomErrorHandlerFunc func(err error, pd *dto.ProblemDetail) error
 
-func GlobalErrorHandler(fn ErrorHandlerFunc) func(c *echo.Context, err error) {
+func GlobalErrorHandler(fn CustomErrorHandlerFunc) echo.HTTPErrorHandler {
 	return func(c *echo.Context, err error) {
-
 		if resp, err := echo.UnwrapResponse(c.Response()); err == nil && resp.Committed {
 			return
 		}
 
-		if fn != nil && fn(c, err) == nil {
-			return
+		pd := dto.ForStatusAndDetail(http.StatusInternalServerError, "Unhandled error at server side").WithProperty("timestamp", time.Now()).WithProperty("trace", "demo-999")
+
+		handled := false
+
+		if fn != nil {
+			handled = fn(err, &pd) == nil
 		}
 
-		handleUncaughtError(c, err)
+		if !handled {
+			handleUnhandledError(err, &pd)
+		}
+
+		c.JSON(pd.Status, pd)
 	}
 }
 
-func handleUncaughtError(c *echo.Context, err error) {
-	pd := dto.ForStatusAndDetail(http.StatusInternalServerError, "Unhandled error at server side")
-
+func handleUnhandledError(err error, pd *dto.ProblemDetail) {
 	switch {
 	case errors.Is(err, constant.ErrInsufficientPermission):
 		pd.Status = http.StatusForbidden
@@ -37,10 +44,10 @@ func handleUncaughtError(c *echo.Context, err error) {
 	case errors.Is(err, ErrMissingRequestHeader):
 		pd.Status = http.StatusBadRequest
 		pd.Detail = err.Error()
-
+	case errors.Is(err, jwt.ErrTokenVerificationFail), errors.Is(err, jwt.ErrTokenClaimsInvalid), errors.Is(err, jwt.ErrTokenMalformed):
+		pd.Status = http.StatusUnauthorized
+		pd.Detail = err.Error()
 	default:
 		slog.Error(err.Error())
 	}
-
-	c.JSON(pd.Status, pd)
 }
