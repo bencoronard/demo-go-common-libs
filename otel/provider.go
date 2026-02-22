cbb58a6f0ad17e5e460fa7daeab2b0d05453e160
ben@orion <128577990+bencoronard@users.noreply.github.com>
2026-02-22 21:17:43 +0700

    Init providers with fx lifecycle

--- otel/provider.go
+++ otel/provider.go
@@ -1,23 +1,59 @@
 package otel
 
 import (
+	"context"
+	"time"
+
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/sdk/log"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/trace"
 	"go.uber.org/fx"
 )
 
-func NewTracerProvider(lc fx.Lifecycle) (*trace.TracerProvider, error) {
-	provider := trace.NewTracerProvider()
+func NewTracerProvider(lc fx.Lifecycle, endpoint string) (*trace.TracerProvider, error) {
+	exporter, err := otlptracegrpc.New(
+		context.Background(),
+		otlptracegrpc.WithInsecure(),
+		otlptracegrpc.WithEndpoint(endpoint),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	provider := trace.NewTracerProvider(
+		trace.WithBatcher(exporter, trace.WithBatchTimeout(time.Second)),
+	)
+
+	lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			return provider.Shutdown(ctx)
+		},
+	})
+
 	return provider, nil
 }
 
 func NewMeterProvider(lc fx.Lifecycle) (*metric.MeterProvider, error) {
 	provider := metric.NewMeterProvider()
+
+	lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			return provider.Shutdown(ctx)
+		},
+	})
+
 	return provider, nil
 }
 
 func NewLoggerProvider(lc fx.Lifecycle) (*log.LoggerProvider, error) {
 	provider := log.NewLoggerProvider()
+
+	lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			return provider.Shutdown(ctx)
+		},
+	})
+
 	return provider, nil
 }
