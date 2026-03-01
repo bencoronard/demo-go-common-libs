e848b90621276a4f42f300fecc237ea62f32a281
ben@orion <128577990+bencoronard@users.noreply.github.com>
2026-03-01 21:06:31 +0700

    Remove manual otel config

--- otel/error.go
+++
@@ -1,7 +0,0 @@
-package otel
-
-import "errors"
-
-var (
-	ErrConstructProvider = errors.New("unable to create a provider")
-)
--- otel/provider.go
+++ otel/provider.go
@@ -2,8 +2,6 @@ package otel
 
 import (
 	"context"
-	"fmt"
-	"time"
 
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
@@ -14,72 +12,47 @@ import (
 	"go.opentelemetry.io/otel/sdk/trace"
 )
 
-func NewTracerProvider(ctx context.Context, res *resource.Resource, endpoint string, batchTimeoutInSec int, samplingRatio float64) (*trace.TracerProvider, error) {
-	exporter, err := otlptracegrpc.New(
-		ctx,
-		otlptracegrpc.WithEndpoint(endpoint),
-		otlptracegrpc.WithInsecure(),
-	)
+func NewTracerProvider(ctx context.Context, res *resource.Resource) (*trace.TracerProvider, error) {
+	exporter, err := otlptracegrpc.New(ctx)
 	if err != nil {
 		return nil, err
 	}
-
-	if samplingRatio < 0.0 || samplingRatio > 1.0 {
-		return nil, fmt.Errorf("%w: trace sampling ratio must be between 0.0 and 1.0", ErrConstructProvider)
-	}
-
-	sampler := trace.ParentBased(trace.TraceIDRatioBased(samplingRatio))
 
 	provider := trace.NewTracerProvider(
 		trace.WithResource(res),
-		trace.WithSampler(sampler),
-		trace.WithBatcher(
-			exporter,
-			trace.WithBatchTimeout(time.Duration(batchTimeoutInSec)*time.Second)),
+		trace.WithBatcher(exporter),
 	)
 
 	return provider, nil
 }
 
-func NewMeterProvider(ctx context.Context, res *resource.Resource, endpoint string, samplingIntervalInSec int) (*metric.MeterProvider, error) {
-	exporter, err := otlpmetricgrpc.New(
-		ctx,
-		otlpmetricgrpc.WithEndpoint(endpoint),
-		otlpmetricgrpc.WithInsecure(),
-	)
+func NewMeterProvider(ctx context.Context, res *resource.Resource) (*metric.MeterProvider, error) {
+	exporter, err := otlpmetricgrpc.New(ctx)
 	if err != nil {
 		return nil, err
 	}
+
+	reader := metric.NewPeriodicReader(exporter)
 
 	provider := metric.NewMeterProvider(
 		metric.WithResource(res),
-		metric.WithReader(metric.NewPeriodicReader(
-			exporter,
-			metric.WithInterval(time.Duration(samplingIntervalInSec)*time.Second),
-		)),
+		metric.WithReader(reader),
 	)
 
 	return provider, nil
 }
 
-func NewLoggerProvider(ctx context.Context, res *resource.Resource, endpoint string, exportIntervalInSec int) (*log.LoggerProvider, error) {
-	exporter, err := otlploggrpc.New(
-		ctx,
-		otlploggrpc.WithEndpoint(endpoint),
-		otlploggrpc.WithInsecure(),
-	)
+func NewLoggerProvider(ctx context.Context, res *resource.Resource) (*log.LoggerProvider, error) {
+	exporter, err := otlploggrpc.New(ctx)
 	if err != nil {
 		return nil, err
 	}
 
+	processor := log.NewBatchProcessor(exporter)
+
 	provider := log.NewLoggerProvider(
 		log.WithResource(res),
-		log.WithProcessor(
-			log.NewBatchProcessor(
-				exporter,
-				log.WithExportInterval(time.Duration(exportIntervalInSec)*time.Second),
-			),
-		),
+		log.WithProcessor(processor),
 	)
 
 	return provider, nil
