370a1ed89c0f43e6760e2140287150f19c3ac129
ben@corp <128577990+bencoronard@users.noreply.github.com>
2026-03-09 21:13:06 +0700

    Try setting span name before next()

--- otel/middleware.go
+++ otel/middleware.go
@@ -2,20 +2,21 @@ package otel
 
 import (
 	"github.com/labstack/echo/v5"
-	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 )
 
 func RouteTagMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c *echo.Context) error {
-		err := next(c)
+		req := c.Request()
+		span := trace.SpanFromContext(req.Context())
 
-		span := trace.SpanFromContext(c.Request().Context())
-		if span.IsRecording() {
-			span.SetName(c.Request().Method + " " + c.Path())
-			span.SetAttributes(attribute.String("http.route", c.Path()))
+		if span != nil {
+			route := c.Path()
+			if route != "" {
+				span.SetName(req.Method + " " + route)
+			}
 		}
 
-		return err
+		return next(c)
 	}
 }
