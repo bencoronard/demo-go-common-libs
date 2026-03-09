faa6ef9814e974e7e62922866d4eaf95bf422082
ben@corp <128577990+bencoronard@users.noreply.github.com>
2026-03-09 20:56:24 +0700

    Try span.SetName in otel middleware

--- otel/middleware.go
+++ otel/middleware.go
@@ -2,15 +2,20 @@ package otel
 
 import (
 	"github.com/labstack/echo/v5"
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 func RouteTagMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c *echo.Context) error {
 		err := next(c)
-		labeler, _ := otelhttp.LabelerFromContext(c.Request().Context())
-		labeler.Add(attribute.String("http.route", c.Path()))
+
+		span := trace.SpanFromContext(c.Request().Context())
+		if span.IsRecording() {
+			span.SetName(c.Request().Method + " " + c.Path())
+			span.SetAttributes(attribute.String("http.route", c.Path()))
+		}
+
 		return err
 	}
 }
