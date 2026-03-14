be0b57439ff3037d68280871d8d24de1065b6c19
ben@corp <128577990+bencoronard@users.noreply.github.com>
2026-03-14 23:38:57 +0700

    Bring back OTEL module

---
+++ otel/provider.go
@@ -0,0 +1,59 @@
+package otel
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+)
+
+func NewTracerProvider(res *resource.Resource) (*trace.TracerProvider, error) {
+	exporter, err := otlptracegrpc.New(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	provider := trace.NewTracerProvider(
+		trace.WithResource(res),
+		trace.WithBatcher(exporter),
+	)
+
+	return provider, nil
+}
+
+func NewMeterProvider(res *resource.Resource) (*metric.MeterProvider, error) {
+	exporter, err := otlpmetricgrpc.New(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	reader := metric.NewPeriodicReader(exporter)
+
+	provider := metric.NewMeterProvider(
+		metric.WithResource(res),
+		metric.WithReader(reader),
+	)
+
+	return provider, nil
+}
+
+func NewLoggerProvider(res *resource.Resource) (*log.LoggerProvider, error) {
+	exporter, err := otlploggrpc.New(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	processor := log.NewBatchProcessor(exporter)
+
+	provider := log.NewLoggerProvider(
+		log.WithResource(res),
+		log.WithProcessor(processor),
+	)
+
+	return provider, nil
+}
