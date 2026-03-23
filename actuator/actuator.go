d251362743b55ead4c28100e1918d7695c718958
ben@corp <128577990+bencoronard@users.noreply.github.com>
2026-03-23 22:59:14 +0700

    Implement actuator constructor

--- actuator/actuator.go
+++ actuator/actuator.go
@@ -1,9 +1,19 @@
 package actuator
 
-import "go.uber.org/fx"
+import (
+	"context"
+	"database/sql"
+	"sync/atomic"
+	"time"
+
+	"go.uber.org/fx"
+	"gorm.io/gorm"
+)
 
 type ActuatorParams struct {
 	fx.In
+	Lifecycle fx.Lifecycle
+	DB        *gorm.DB `optional:"true"`
 }
 
 type Actuator interface {
@@ -12,10 +22,38 @@ type Actuator interface {
 }
 
 type actuatorImpl struct {
+	ready atomic.Bool
+	db    *sql.DB
 }
 
-func NewActuator(p ActuatorParams) Actuator {
-	return &actuatorImpl{}
+func NewActuator(p ActuatorParams) (Actuator, error) {
+	var err error
+	var sqlDB *sql.DB
+	if p.DB != nil {
+		sqlDB, err = p.DB.DB()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	a := &actuatorImpl{
+		db: sqlDB,
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	p.Lifecycle.Append(fx.Hook{
+		OnStart: func(_ context.Context) error {
+			go a.monitor(ctx)
+			return nil
+		},
+		OnStop: func(_ context.Context) error {
+			cancel()
+			return nil
+		},
+	})
+
+	return a, nil
 }
 
 func (a *actuatorImpl) Liveness() bool {
@@ -23,5 +61,23 @@ func (a *actuatorImpl) Liveness() bool {
 }
 
 func (a *actuatorImpl) Readiness() bool {
-	return true
+	return a.ready.Load()
+}
+
+func (a *actuatorImpl) monitor(ctx context.Context) {
+	ticker := time.NewTicker(15 * time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			err := a.check(ctx)
+			a.ready.Store(err == nil)
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (a *actuatorImpl) check(ctx context.Context) error {
+	return a.db.PingContext(ctx)
 }
