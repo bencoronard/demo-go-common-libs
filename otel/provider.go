6b5d4549cdadc7eb325fb8a98026b9be456f8d5d
ben@corp <128577990+bencoronard@users.noreply.github.com>
2026-03-31 14:49:01 +0700

    Remove setup function in otel module

--- otel/provider.go
+++ otel/provider.go
@@ -3,30 +3,59 @@ package otel
 import (
 	"context"
 
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/log/global"
+	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/log"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
+	"go.uber.org/fx"
 )
 
-func NewTracerProvider(res *resource.Resource) (*trace.TracerProvider, error) {
+type OtelParams struct {
+	fx.In
+	Lc   fx.Lifecycle
+	Prop propagation.TextMapPropagator
+	Res  *resource.Resource
+}
+
+func NewPropagator() (propagation.TextMapPropagator, error) {
+	prop := propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+	otel.SetTextMapPropagator(prop)
+
+	return prop, nil
+}
+
+func NewTracerProvider(p OtelParams) (*trace.TracerProvider, error) {
 	exporter, err := otlptracegrpc.New(context.Background())
 	if err != nil {
 		return nil, err
 	}
 
 	provider := trace.NewTracerProvider(
-		trace.WithResource(res),
+		trace.WithResource(p.Res),
 		trace.WithBatcher(exporter),
 	)
+
+	otel.SetTracerProvider(provider)
+
+	p.Lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			return provider.Shutdown(ctx)
+		},
+	})
 
 	return provider, nil
 }
 
-func NewMeterProvider(res *resource.Resource) (*metric.MeterProvider, error) {
+func NewMeterProvider(p OtelParams) (*metric.MeterProvider, error) {
 	exporter, err := otlpmetricgrpc.New(context.Background())
 	if err != nil {
 		return nil, err
@@ -35,14 +64,22 @@ func NewMeterProvider(res *resource.Resource) (*metric.MeterProvider, error) {
 	reader := metric.NewPeriodicReader(exporter)
 
 	provider := metric.NewMeterProvider(
-		metric.WithResource(res),
+		metric.WithResource(p.Res),
 		metric.WithReader(reader),
 	)
+
+	otel.SetMeterProvider(provider)
+
+	p.Lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			return provider.Shutdown(ctx)
+		},
+	})
 
 	return provider, nil
 }
 
-func NewLoggerProvider(res *resource.Resource) (*log.LoggerProvider, error) {
+func NewLoggerProvider(p OtelParams) (*log.LoggerProvider, error) {
 	exporter, err := otlploggrpc.New(context.Background())
 	if err != nil {
 		return nil, err
@@ -51,9 +88,17 @@ func NewLoggerProvider(res *resource.Resource) (*log.LoggerProvider, error) {
 	processor := log.NewBatchProcessor(exporter)
 
 	provider := log.NewLoggerProvider(
-		log.WithResource(res),
+		log.WithResource(p.Res),
 		log.WithProcessor(processor),
 	)
+
+	global.SetLoggerProvider(provider)
+
+	p.Lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			return provider.Shutdown(ctx)
+		},
+	})
 
 	return provider, nil
 }
