6a4b128f6ef5753282c0e9e9e43f017d2758b39a
ben@corp <128577990+bencoronard@users.noreply.github.com>
2026-04-25 15:32:16 +0700

    Add optional actuator in server module

--- server/provider.go
+++ server/provider.go
@@ -8,14 +8,16 @@ import (
 	"os"
 	"time"
 
+	"github.com/bencoronard/demo-go-common-libs/actuator"
 	"go.uber.org/fx"
 	"google.golang.org/grpc"
 )
 
 type ServerParams struct {
 	fx.In
-	Lc fx.Lifecycle
-	Sd fx.Shutdowner
+	Lc  fx.Lifecycle
+	Sd  fx.Shutdowner
+	Act actuator.Actuator `optional:"true"`
 }
 
 type HttpServer interface {
