7d876c88b3968bd41fcf745ee6811f7976d44824
ben@corp <128577990+bencoronard@users.noreply.github.com>
2026-04-26 16:09:11 +0700

    Remove vault errors

--- vault/error.go
+++ vault/error.go
@@ -2,8 +2,4 @@ package vault
 
 import "errors"
 
-var (
-	ErrConfigUnset        = errors.New("required vault config unset")
-	ErrSecretNotFound     = errors.New("secret not unavailable")
-	ErrAuthenticationFail = errors.New("vault authentication fail")
-)
+var ErrSecretNotFound = errors.New("secret not found")
