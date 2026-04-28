6442a5ec1806da72a39c0b60da91b4aad05fed96
ben@corp <128577990+bencoronard@users.noreply.github.com>
2026-04-28 21:00:57 +0700

    Add default logger, validator, panic middleware to echo router

--- http/router.go
+++ http/router.go
@@ -1,22 +1,37 @@
 package http
 
 import (
+	"log/slog"
+
 	"github.com/bencoronard/demo-go-common-libs/validator"
 	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
 	"go.uber.org/fx"
 )
 
 type echoRouterParams struct {
 	fx.In
 	ErrHandler GlobalErrorHandler
+	Logger     *slog.Logger        `optional:"true"`
 	Val        validator.Validator `optional:"true"`
 }
 
 func NewEchoRouter(p echoRouterParams) *echo.Echo {
 	e := echo.New()
+
 	e.HTTPErrorHandler = p.ErrHandler.GetHandler()
+
+	if p.Logger != nil {
+		e.Logger = p.Logger
+	}
+
 	if p.Val != nil {
 		e.Validator = p.Val
 	}
+
+	e.Use(
+		middleware.Recover(),
+	)
+
 	return e
 }
