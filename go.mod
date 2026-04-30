1441d068436f91afcf6a34f18f380387117be32f
ben@corp <128577990+bencoronard@users.noreply.github.com>
2026-04-30 22:16:42 +0700

    Add middleware configurations to echo router

--- go.mod
+++ go.mod
@@ -51,6 +51,7 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/labstack/echo-opentelemetry v0.0.2
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
@@ -60,14 +61,14 @@ require (
 	go.opentelemetry.io/proto/otlp v1.10.0 // indirect
 	go.uber.org/dig v1.19.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.27.1 // indirect
+	go.uber.org/zap v1.28.0 // indirect
 	golang.org/x/crypto v0.50.0 // indirect
 	golang.org/x/net v0.53.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.43.0 // indirect
 	golang.org/x/text v0.36.0 // indirect
 	golang.org/x/time v0.15.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20260420184626-e10c466a9529 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20260420184626-e10c466a9529 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20260427160629-7cedc36a6bc4 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260427160629-7cedc36a6bc4 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 )
--- http/router.go
+++ http/router.go
@@ -1,19 +1,35 @@
 package http
 
 import (
+	"cmp"
 	"log/slog"
+	"slices"
+	"strings"
 
 	"github.com/bencoronard/demo-go-common-libs/validator"
+	echootel "github.com/labstack/echo-opentelemetry"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/trace"
 	"go.uber.org/fx"
 )
 
+type RouterConfig struct {
+	ContextPath     string
+	EnableAccessLog bool
+}
+
 type echoRouterParams struct {
 	fx.In
+	Cfg        RouterConfig
 	ErrHandler GlobalErrorHandler
-	Logger     *slog.Logger        `optional:"true"`
-	Val        validator.Validator `optional:"true"`
+	Logger     *slog.Logger                  `optional:"true"`
+	Val        validator.Validator           `optional:"true"`
+	Pp         propagation.TextMapPropagator `optional:"true"`
+	Tp         *trace.TracerProvider         `optional:"true"`
+	Mp         *metric.MeterProvider         `optional:"true"`
 }
 
 func NewEchoRouter(p echoRouterParams) *echo.Echo {
@@ -29,9 +45,92 @@ func NewEchoRouter(p echoRouterParams) *echo.Echo {
 		e.Validator = p.Val
 	}
 
-	e.Use(
+	preMiddlewares := []echo.MiddlewareFunc{
+		contextPathMiddleware(p.Cfg.ContextPath),
+	}
+
+	middlewares := []echo.MiddlewareFunc{
 		middleware.Recover(),
-	)
+		otelMiddleware(p.Pp, p.Tp, p.Mp),
+		accessLogMiddleware(p.Logger, p.Cfg.EnableAccessLog),
+	}
+
+	e.Pre(compact(preMiddlewares)...)
+	e.Use(compact(middlewares)...)
 
 	return e
+}
+
+func compact(mws []echo.MiddlewareFunc) []echo.MiddlewareFunc {
+	return slices.DeleteFunc(mws, func(mw echo.MiddlewareFunc) bool {
+		return mw == nil
+	})
+}
+
+func contextPathMiddleware(contextPath string) echo.MiddlewareFunc {
+	if contextPath == "" {
+		return nil
+	}
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c *echo.Context) error {
+			if path, ok := strings.CutPrefix(c.Request().URL.Path, contextPath); ok {
+				c.Request().URL.Path = cmp.Or(path, "/")
+			}
+			return next(c)
+		}
+	}
+}
+
+func otelMiddleware(pp propagation.TextMapPropagator, tp *trace.TracerProvider, mp *metric.MeterProvider) echo.MiddlewareFunc {
+	if tp == nil && mp == nil {
+		return nil
+	}
+	return echootel.NewMiddlewareWithConfig(echootel.Config{
+		Propagators:    pp,
+		TracerProvider: tp,
+		MeterProvider:  mp,
+	})
+}
+
+func accessLogMiddleware(logger *slog.Logger, enabled bool) echo.MiddlewareFunc {
+	if !enabled {
+		return nil
+	}
+	return middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogLatency:      true,
+		LogProtocol:     true,
+		LogRemoteIP:     true,
+		LogMethod:       true,
+		LogURI:          true,
+		LogStatus:       true,
+		LogResponseSize: true,
+		LogUserAgent:    true,
+
+		LogValuesFunc: func(c *echo.Context, v middleware.RequestLoggerValues) error {
+			attrs := []slog.Attr{
+				slog.String("protocol", v.Protocol),
+				slog.String("remote_ip", v.RemoteIP),
+				slog.String("method", v.Method),
+				slog.String("uri", v.URI),
+				slog.Int("status", v.Status),
+				slog.Duration("latency", v.Latency),
+				slog.Int64("response_size", v.ResponseSize),
+				slog.String("user_agent", v.UserAgent),
+			}
+
+			level := slog.LevelInfo
+			switch {
+			case v.Error != nil:
+				level = slog.LevelError
+				attrs = append(attrs, slog.Any("error", v.Error))
+			case v.Status >= 500:
+				level = slog.LevelError
+			case v.Status >= 400:
+				level = slog.LevelWarn
+			}
+
+			logger.LogAttrs(c.Request().Context(), level, "request", attrs...)
+			return nil
+		},
+	})
 }
