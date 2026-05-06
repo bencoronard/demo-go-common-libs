8b6ee43630b59e9d350526efc6a7cefbb028d964
ben@corp <128577990+bencoronard@users.noreply.github.com>
2026-05-06 21:54:57 +0700

    Init vault error revamp

--- vault/error.go
+++ vault/error.go
@@ -2,4 +2,7 @@ package vault
 
 import "errors"
 
-var ErrSecretNotFound = errors.New("secret not found")
+var (
+	ErrSecretNotFound = errors.New("secret not found")
+	ErrConfig         = errors.New("")
+)
